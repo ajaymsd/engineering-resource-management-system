2dd20a8bbef5d2e94c86b39e4da8be00b416cdc1
ajaymsd <ajbalasubramanian2002@gmail.com>
2025-07-04 07:29:07 +0530

    added vercel support

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,6 +13,10 @@ app.use(express.json());
 
 app.use('/api/v1/auth', authRoutes);
 
+app.get('/',(req,res)=>{
+  res.status(200).json("Welcome to the App");
+});
+
 app.use('/api/v1/engineers', authMiddleware, engineerRoutes);
 app.use('/api/v1/projects', authMiddleware, projectRoutes);
 app.use('/api/v1/assignments', authMiddleware, assignmentRoutes);
